bb3a7227c864f05abef75d0938d9cca0f7c489cc
Travis Deig <travisdeig@gmail.com>
2020-07-03 12:07:00 -0400

    Footer links, sticky top header, and index column fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-$(function) () {
-    $()
-}
+$(function() {
+    $("#pwButton").click(function () {
+        $("#pwModal").modal("show");
+    });
+    $("#rcButton").click(function () {
+        $("#rcModal").modal("show");
+    });
+    $("#gcButton").click(function () {
+        $("#gcModal").modal("show");
+    });
+    $("#wwButton").click(function () {
+        $("#wwModal").modal("show");
+    });
+    $("#psButton").click(function () {
+        $("#psModal").modal("show");
+    });
+});
